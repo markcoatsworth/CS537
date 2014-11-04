ee3c3a25b109ce2f8641ee03910cc80a3685cb2f
Mark Coatsworth <coatsworth@macaroni-01.cs.wisc.edu>
2014-11-04 12:14:48 -0600

    P4b basic threaded web server

--- p4/linux/server.c
+++ p4/linux/server.c
@@ -13,19 +13,33 @@
 
 
 /*
-**	Global variables: these will be shared between all threads and the main process
+**	HTTP connection struct
+*/
+/*
+typedef struct HttpConnection 
+{
+	int ConnectionSocket;
+}
+HttpConnection;
 */
 
+/*
+**	Global variables: these will be shared between all threads and the main process
+*/
+//HttpConnection *HttpConnectionBuffers;
+int ActiveConnection = 0;
+int *HttpConnections;
 int NumBuffers;
 int NumThreads;
 int PortNumber;
+int ThreadCounter = 0;
 pthread_t *ThreadPool;
+pthread_mutex_t ConnectionBufferLock;
 
 
 /*
 **	Get command line arguments and save them to relevant pointers. Exit if any problems.
 */
-
 void getargs(int argc, char *argv[])
 {
     if (argc != 4) 
@@ -40,35 +54,59 @@ void getargs(int argc, char *argv[])
 
 
 /*
-**
+**	Thread function to handle HTTP connections
 */
-
 void HttpConnectionHandler()
 {
-	printf("[HttpConnectionHandler] called\n");
+	int ThisThreadIndex = ThreadCounter++;
+	
+	printf("[HttpConnectionHandler-%d] thread started\n", ThisThreadIndex);
+
+	while(1)
+	{
+		printf("[HttpConnectionHandler-%d] requesting lock\n", ThisThreadIndex);
+		if(pthread_mutex_lock(&ConnectionBufferLock) != 0)
+		{
+			printf("[HttpConnectionHandler-%d] unable to get lock\n", ThisThreadIndex);
+		}
+		printf("[HttpConnectionHandler-%d] thread unlocked!\n", ThisThreadIndex);
+		
+		printf("[HttpConnectionHandler-%d] calling request handle on fd=%d...\n", ThisThreadIndex, HttpConnections[ActiveConnection]);
+		//requestHandle(ConnectionSocket);
+		requestHandle(HttpConnections[ActiveConnection]);
+		Close(HttpConnections[ActiveConnection]);
+		ActiveConnection++;
+		
+	}
 }
 
 
 /*
 **	Main server program
 */
-
 int main(int argc, char *argv[])
 {
-    int HttpListenFD, connfd, clientlen;
-    int i;
-    
+	// Define variables
+    int HttpListenSocket, ConnectionSocket, clientlen;
+    int i;    
     struct sockaddr_in clientaddr;
+    
+    // Initialize some global variables
+
 
 	// Verify + store command line arguments
     getargs(argc, argv);
 	printf("[server] Running with NumBuffers=%d, NumThreads=%d, PortNumber=%d\n", NumBuffers, NumThreads, PortNumber);
 
+	// Set up connection buffers
+	//HttpConnectionBuffers = (HttpConnectionBuffers*) malloc (sizeof(HttpConnectionBuffers*) * NumBuffers);
+	HttpConnections = (int *) malloc (sizeof(int*) * NumBuffers);
+
     // Set up the thread pool, and initialize all threads
     ThreadPool = (pthread_t*) malloc (sizeof(pthread_t*) * NumThreads);
     for(i = 0; i < NumThreads; i ++)
     {
-   		printf("[server] creating new thread\n");
+   		//printf("[server] creating new thread\n");
     	if(pthread_create(&ThreadPool[i], NULL, &HttpConnectionHandler, NULL) != 0)
     	{
     		perror("Failed to create HTTP connection handler thread.\n");
@@ -76,7 +114,7 @@ int main(int argc, char *argv[])
     }
 
 	// Open the HTTP connection
-    HttpListenFD = Open_listenfd(PortNumber);
+    HttpListenSocket = Open_listenfd(PortNumber);
 
     // Main server loop waiting for connections
     while (1) 
@@ -84,17 +122,22 @@ int main(int argc, char *argv[])
     	printf("[server] clientlen=%d\n", clientlen);
 		clientlen = sizeof(clientaddr);
 		printf("[server] waiting for connection...\n");
-		connfd = Accept(HttpListenFD, (SA *)&clientaddr, (socklen_t *) &clientlen);
+		ConnectionSocket = Accept(HttpListenSocket, (SA *)&clientaddr, (socklen_t *) &clientlen);
 	
 		// 
 		// CS537: In general, don't handle the request in the main thread.
 		// Save the relevant info in a buffer and have one of the worker threads 
 		// do the work.
-		// 
-		printf("[server] calling request handle...\n");
-		requestHandle(connfd);
-	
-		Close(connfd);
+		// requestHandle(ConnectionSocket);
+		
+		printf("[server] got a connection at fd=%d! saving connection data to buffer\n", ConnectionSocket);
+		HttpConnections[ActiveConnection] = ConnectionSocket;
+		printf("[server] releasing lock\n");
+		if(pthread_mutex_unlock(&ConnectionBufferLock) != 0)
+		{
+			printf("[server] unable to release lock\n");
+		}		
+
     }
 
 }
